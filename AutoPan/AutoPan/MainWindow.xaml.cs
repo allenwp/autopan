36ed69e73624ce66b89439639e52997864f7d72a
Allen Pestaluky <allenpestaluky@gmail.com>
2016-03-26 11:26:04 -0400

    Iterated some more on the welcome message to make setup more clear.

--- AutoPan/AutoPan/MainWindow.xaml.cs
+++ AutoPan/AutoPan/MainWindow.xaml.cs
@@ -111,7 +111,7 @@ namespace AutoPan
 
             Closing += MainWindow_Closing;
 
-            OnLogMessage(this, new LogMessageEventArgs(LogSeverity.Info, "Auto Pan", "Thanks for using Auto Pan! Here's how to set it up: \n\n- Auto Pan requires a secondary Discord user account that has access to the server and voice channel you are using with your primary account. Use this second account to log in above. I recommend adding \"[Auto Pan]\" to the secondary account's username.\n\n- Use Discord normally with your primary account for transmitting your voice.\n\n- Auto Pan will output panned audio from the connected voice channel.\n\n- Mute each individual user in Discord to prevent duplicate audio/echo.\n\nThis is version 1 – Keep an eye out for new updates at: allenwp.github.io/autopan", null));
+            OnLogMessage(this, new LogMessageEventArgs(LogSeverity.Info, "Auto Pan", "Thanks for using Auto Pan! Here's how to set it up: \n\n- Auto Pan requires you to create a second Discord user account. I recommend adding \"[Auto Pan]\" it's username.\n\n-Your second account needs access to the server and voice channel you are using with your primary account.\n\n-Use this second account to log in above.\n\n- Auto Pan will output panned audio from the voice channel it's connected to.\n\n- Use Discord normally with your primary account for transmitting your voice.\n\n- Mute each individual user in Discord to prevent duplicate audio/echo.\n\nThis is version 1 – Keep an eye out for new updates at: allenwp.github.io/autopan", null));
             logScrollViewer.ScrollToTop();
             
             try
