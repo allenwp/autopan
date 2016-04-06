9a5f89c3c5d74bc7a94433c97553057478c8f8f5
Allen Pestaluky <allenpestaluky@gmail.com>
2016-04-06 00:16:59 -0400

    Changed version number to 0.1. Added some other assembly info

--- AutoPan/AutoPan/Properties/AssemblyInfo.cs
+++ AutoPan/AutoPan/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoPan")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Auto Pan")]
+[assembly: AssemblyDescription("An auto-pan for Discord. Performs an automatic stereo spread of user's voices in a Discord voice channel.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoPan")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Auto Pan")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Allen Pestaluky")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
